f652304c60af9dddd187b8499ff04428c390ef83
IAMKOTARO <saitou.kotarou898@mail.kyutech.jp>
2021-06-28 19:40:30 +0900

    Initial Commit

---
+++ BicycleSupporter/BicycleSupporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BicycleSupporterApp.swift
+//  BicycleSupporter
+//
+//  Created by 斉藤虎太郎 on 2021/06/28.
+//
+
+import SwiftUI
+
+@main
+struct BicycleSupporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BicycleSupporter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BicycleSupporter
+//
+//  Created by 斉藤虎太郎 on 2021/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
