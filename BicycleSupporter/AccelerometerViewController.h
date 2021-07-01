52f765d4b8c35032e84fc956623345da37b81c7b
IAMKOTARO <saitou.kotarou898@mail.kyutech.jp>
2021-07-01 22:01:01 +0900

    first commit

---
+++ BicycleSupporter/AccelerometerViewController.h
@@ -0,0 +1,12 @@
+//
+//  AccelerometerViewController.h
+//  BicycleSupporter
+//
+//  Created by 斉藤虎太郎 on 2021/06/28.
+//
+
+#ifndef AccelerometerViewController_h
+#define AccelerometerViewController_h
+
+
+#endif /* AccelerometerViewController_h */
---
+++ BicycleSupporter/AccelerometerViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AccelerometerViewController.swift
+//  BicycleSupporter
+//
+//  Created by 斉藤虎太郎 on 2021/06/28.
+//
+
+import Foundation
