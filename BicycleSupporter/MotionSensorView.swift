3d5700ceb716252fbe9520d480776cce7a52984a
IAMKOTARO <saitou.kotarou898@mail.kyutech.jp>
2021-08-18 20:58:23 +0900

    :+1:set experiment name

--- BicycleSupporter/MotionSensorView.swift
+++ BicycleSupporter/MotionSensorView.swift
@@ -12,22 +12,49 @@ struct MotionSensorView: View {
     @ObservedObject var motionSensor = MotionSensor()
     var body: some View {
         VStack {
+            Text("実験の名前を入力してください")
+            TextField("実験の名前", text: $motionSensor.experimentName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
             Text("status: \(motionSensor.state)")
-            Text("Accelometer")
-                .padding()
-            Text("x = \(motionSensor.xAcc)")
-            Text("y = \(motionSensor.yAcc)")
-            Text("z = \(motionSensor.zAcc)")
-            Text("Gyro")
-                .padding()
-            Text("x = \(motionSensor.xGyro)")
-            Text("y = \(motionSensor.yGyro)")
-            Text("z = \(motionSensor.zGyro)")
+            HStack {
+                VStack {
+                    Text("Accelometer")
+                        .padding()
+                    Text("x = \(motionSensor.xAcc)")
+                    Text("y = \(motionSensor.yAcc)")
+                    Text("z = \(motionSensor.zAcc)")
+                }
+                VStack {
+                    Text("Gyro")
+                        .padding()
+                    Text("x = \(motionSensor.xGyro)")
+                    Text("y = \(motionSensor.yGyro)")
+                    Text("z = \(motionSensor.zGyro)")
+                }
+            }
+            HStack {
+                VStack {
+                    Text("Gravity")
+                        .padding()
+                    Text("x = \(motionSensor.xGrav)")
+                    Text("y = \(motionSensor.yGrav)")
+                    Text("z = \(motionSensor.zGrav)")
+                }
+                VStack {
+                    Text("Attitude")
+                        .padding()
+                    Text("x = \(motionSensor.xAtt)")
+                    Text("y = \(motionSensor.yAtt)")
+                    Text("z = \(motionSensor.zAtt)")
+                }
+            }
             Button(action: {
                 self.motionSensor.isStarted ? self.motionSensor.stop() : self.motionSensor.start()
             }) {
                 self.motionSensor.isStarted ? Text("STOP").padding() : Text("START").padding()
             }
+            .disabled(motionSensor.experimentName.isEmpty)
         }
     }
 }
@@ -46,15 +73,25 @@ class MotionSensor: NSObject, ObservableObject {
     @Published var xGyro = "0.0"
     @Published var yGyro = "0.0"
     @Published var zGyro = "0.0"
+    @Published var xGrav = "0.0"
+    @Published var yGrav = "0.0"
+    @Published var zGrav = "0.0"
+    @Published var xAtt = "0.0"
+    @Published var yAtt = "0.0"
+    @Published var zAtt = "0.0"
     @Published var time = 0.0
     @Published var state = "waiting"
+    @Published var experimentName = ""
     var startDate = Date()
     var endDate = Date()
     var accelerationArrData: [[Double]] = [[Double]]()
     var gyroArrData: [[Double]] = [[Double]]()
+    var gravArrData: [[Double]] = [[Double]]()
+    var attArrData: [[Double]] = [[Double]]()
     let motionManager = CMMotionManager()
     
     func start() {
+        self.experimentName = experimentName
         if motionManager.isDeviceMotionAvailable {
             state = "running"
             startDate = Date()
@@ -74,9 +111,11 @@ class MotionSensor: NSObject, ObservableObject {
         motionManager.stopDeviceMotionUpdates()
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yMMMdHms", options: 0, locale: Locale(identifier: "ja_JP"))
-        let fileName = dateFormatter.string(from: startDate) + "~" + dateFormatter.string(from: endDate)
-        saveToCsv(fileName: "\(fileName)/acc", fileArrData: accelerationArrData)
-        saveToCsv(fileName: "\(fileName)/gyro", fileArrData: gyroArrData)
+        let fileName = self.experimentName + "_" + dateFormatter.string(from: startDate) + "~" + dateFormatter.string(from: endDate)
+        saveToLocalCsv(fileName: "\(fileName)-acc", fileArrData: accelerationArrData)
+        saveToLocalCsv(fileName: "\(fileName)-gyro", fileArrData: gyroArrData)
+        saveToLocalCsv(fileName: "\(fileName)-grav", fileArrData: gravArrData)
+        saveToLocalCsv(fileName: "\(fileName)-att", fileArrData: attArrData)
     }
 
     func updateMotionData(deviceMotion: CMDeviceMotion) {
@@ -88,11 +127,19 @@ class MotionSensor: NSObject, ObservableObject {
         yGyro = String(deviceMotion.rotationRate.y)
         zGyro = String(deviceMotion.rotationRate.z)
         gyroArrData.append([atof(xGyro), atof(yGyro), atof(zGyro)])
+        xGrav = String(deviceMotion.gravity.x)
+        yGrav = String(deviceMotion.gravity.y)
+        zGrav = String(deviceMotion.gravity.z)
+        gravArrData.append([atof(xGrav), atof(yGrav), atof(zGrav)])
+        xAtt = String(deviceMotion.attitude.pitch)
+        yAtt = String(deviceMotion.attitude.roll)
+        zAtt = String(deviceMotion.attitude.yaw)
+        attArrData.append([atof(xAtt), atof(yAtt), atof(zAtt)])
         time += 0.1
     }
     
     //多次元配列からDocuments下にCSVファイルを作る
-    func saveToCsv(fileName : String, fileArrData : [[Double]]){
+    func saveToLocalCsv(fileName : String, fileArrData : [[Double]]){
         // save acc data to csv
         let filePath = NSHomeDirectory() + "/Documents/" + fileName + ".csv"
         var fileStrData:String = "x,y,z\n"
